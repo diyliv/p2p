d80174d703db76ff0b5041137ebb4c8232ed8f8e
diyliv <onlogn081@gmail.com>
2026-01-17 17:53:21 +0300

    reading values from terminal and incase of any errors logging them

--- cmd/p2pchat/main.go
+++ cmd/p2pchat/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s <listen-port> ./app 8080 (for example)\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	var port int
+	_, err := fmt.Sscanf(os.Args[1], "%d", &port)
+	if err != nil {
+		log.Fatalf("Invalid port: %v", err)
+	}
 }
