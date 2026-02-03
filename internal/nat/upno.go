c1c429dacc8455fd4e60dc87a6d226fb41719e35
diyliv <onlogn081@gmail.com>
2026-02-03 14:02:36 +0300

    define structure for upnportforwarder

--- internal/nat/upno.go
+++ internal/nat/upno.go
@@ -1,1 +1,18 @@
 package nat
+
+import (
+	"net"
+
+	"github.com/huin/goupnp/dcps/internetgateway2"
+)
+
+type UPnPPortForwarder struct {
+	client     *internetgateway2.WANIPConnection1
+	internalIP net.IP
+	externalIP net.IP
+	port       int
+}
+
+func NewUPnPPortForwarder(port int) (*UPnPPortForwarder, error) {
+	return nil, nil
+}
